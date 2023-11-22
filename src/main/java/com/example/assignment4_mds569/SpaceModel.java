655d90e8b77841a492e5fdc32996fd1a601c3f93
Matthew S <mds569@usask.ca>
2023-11-21 20:29:12 -0600

    Updated SpaceModel.java to use same structure as previous assignment, but holding both Star and Asteroid objects

--- src/main/java/com/example/assignment4_mds569/SpaceModel.java
+++ src/main/java/com/example/assignment4_mds569/SpaceModel.java
@@ -1,8 +1,70 @@
 package com.example.assignment4_mds569;
 
+import java.util.ArrayList;
+
 public class SpaceModel {
 
+    private ArrayList<Asteroid> asteroids;
+
+    private ArrayList<Star> stars;
+
     PublishSubscribe pubSub;
+
+    public SpaceModel()
+    {
+        asteroids = new ArrayList<Asteroid>();
+        stars = new ArrayList<Star>();
+    }
+
+    // Create new Asteroid object
+    public void createAsteroid()
+    {
+        Asteroid asteroid = new Asteroid();
+        asteroids.add(asteroid);
+        pubSub.publish("create");
+    }
+
+    // Delete an Asteroid at a specific index
+    public void deleteAsteroid(int index)
+    {
+        if (index <= asteroids.size())
+        {
+            asteroids.remove(index);
+            pubSub.publish("delete");
+        }
+    }
+
+    // Return list of Asteroids
+    public ArrayList<Asteroid> getAsteroids() {return this.asteroids;}
+
+    // Return Asteroid at index
+    public Asteroid getAsteroid(int index) {return this.asteroids.get(index);}
+
+    // Create new Star object
+    public void createStar()
+    {
+        Star star = new Star();
+        stars.add(star);
+        pubSub.publish("create");
+    }
+
+    // Delete a Star at a specific index
+    public void deleteStar(int index)
+    {
+        if (index <= stars.size())
+        {
+            stars.remove(index);
+            pubSub.publish("delete");
+        }
+    }
+
+    // Return list of Stars
+    public ArrayList<Star> getStars() {return this.stars;}
+
+    // Return Star at index
+    public Star getStar(int index) {return this.stars.get(index);}
+
+    // Set the PublishSubscribe object for this model
     public void setPubSub(PublishSubscribe publishSubscribe) {
         this.pubSub = publishSubscribe;
     }
