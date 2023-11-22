f84c2254b9054a5035b014b3d111bc68a1002f76
Matthew S <mds569@usask.ca>
2023-11-21 20:42:39 -0600

    Created skeleton for InteractionModel.java similar to previous assignment

--- src/main/java/com/example/assignment4_mds569/InteractionModel.java
+++ src/main/java/com/example/assignment4_mds569/InteractionModel.java
@@ -2,6 +2,11 @@ package com.example.assignment4_mds569;
 
 public class InteractionModel {
     private PublishSubscribe pubSub;
+
+    public InteractionModel(){
+
+    }
+
     public void setPubSub(PublishSubscribe publishSubscribe) {
         this.pubSub = publishSubscribe;
     }
