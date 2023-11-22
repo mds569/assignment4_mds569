ef423a5989beda602addb410cce28c50c3b311c2
Matthew S <mds569@usask.ca>
2023-11-21 21:15:04 -0600

    Created temporary draw operation for Part 1 of the assignment

--- src/main/java/com/example/assignment4_mds569/SpaceView.java
+++ src/main/java/com/example/assignment4_mds569/SpaceView.java
@@ -3,15 +3,19 @@ package com.example.assignment4_mds569;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
 public class SpaceView extends StackPane implements Subscriber {
     private InteractionModel iModel;
     private SpaceModel model;
 
+    private double canvasSize;
+
     public Canvas myCanvas;
     public GraphicsContext graphicsContext;
 
     public SpaceView(double size) {
+        this.canvasSize = size;
         myCanvas = new Canvas(size, size);
         graphicsContext = myCanvas.getGraphicsContext2D();
         this.setStyle("-fx-background-color: black;");
@@ -41,6 +45,15 @@ public class SpaceView extends StackPane implements Subscriber {
     }
 
     public void draw() {
+        graphicsContext.clearRect(0, 0, canvasSize, canvasSize);
+        graphicsContext.setFill(Color.WHITE);
+        for(Star star : model.getStars()){
+            graphicsContext.fillOval(star.getX(), star.getY(), star.getRadius(),star.getRadius());
+        }
+        graphicsContext.setFill(Color.GRAY);
+        for (Asteroid asteroid : model.getAsteroids()){
+            graphicsContext.fillOval(asteroid.getX(), asteroid.getY(), asteroid.getRadius(), asteroid.getRadius());
+        }
         System.out.println("Draw");
     }
 
